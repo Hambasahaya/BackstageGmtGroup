63985575faf00b290683ac386ebc7543bc62240f
Hambasahaya <zaldyramlan24@gmail.com>
2026-07-01 13:46:10 +0700

    fix bug error:id card models

--- vite.config.ts
+++ vite.config.ts
@@ -13,20 +13,5 @@ export default defineConfig({
   assetsInclude: ['**/*.svg', '**/*.csv'],
   build: {
     chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('recharts') || id.includes('d3')) {
-              return 'vendor-recharts';
-            }
-            if (id.includes('lucide-react')) {
-              return 'vendor-lucide';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
   },
 })
